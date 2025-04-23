193ea2cc6e01f0d298a5e71e092e0e87d1817ff8
aniag <251142@edu.p.lodz.pl>
2025-04-23 20:21:41 +0200

    husunięcie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -19,7 +19,4 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
         return totalRejected;
     }
 
-    public void setTotalRejected(int totalRejected) {
-        this.totalRejected = totalRejected;
-    }
 }
